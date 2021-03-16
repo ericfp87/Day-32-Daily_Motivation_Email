9cf9fd3bbc5c9fe3691d89b8ea183027f4b6cc91
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-03-16 10:18:09 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import smtplib
+import datetime as dt
+import pandas as pd
+import random
+
+
+#E-mail
+# my_email = "appbreweryinfo@gmail.com"
+# password = "abcd1234()"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs="appbrewerytesting@yahoo.com",
+#                         msg="Subject:Hello!!\n\nThis is the body of my email"
+#     )
+
+
+#Datetime
+date = dt.datetime.now()
+weekday = date.weekday()
+
+
+#Quotes
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+MY_EMAIL = "teste@gmail.com"
+MY_PASSWORD = "teste"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=MY_EMAIL,
+                            msg=f"Subject:Motivation\n\n{quote}"
+        )
